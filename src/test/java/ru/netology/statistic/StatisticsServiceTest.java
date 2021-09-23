9c9b9a954e6127b530fca833869760629127f41d
safaleks <safaleks@mail.ru>
2021-09-24 01:53:28 +0300

    add jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,9 +1,8 @@
-
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
